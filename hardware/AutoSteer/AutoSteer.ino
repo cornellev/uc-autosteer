9b4fe55cb5a36b294be9f4ca679e10c1563159f2
erica! <115647009+yuchenie@users.noreply.github.com>
2025-02-25 20:26:34 -0500

    Update AutoSteer.ino

--- hardware/AutoSteer/AutoSteer.ino
+++ hardware/AutoSteer/AutoSteer.ino
@@ -1,20 +1,35 @@
-const int PUL = 3;
-const int DIR = 2;
-const int MAX_DUTY_CYCLE = 0.15;
-float output = 0;
+#define PUL 3
+#define DIR 2
+#define STEERING_SENSOR A5
+const float MAX_DUTY_CYCLE = 0.15;
+
+float output = 0.5;
+float time_low = 3;
+float time_high = time_low / (output * MAX_DUTY_CYCLE);
+float timer;
+float last_time = 0;
+float current_time = 0;
+float v_out = HIGH;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
-  digitalWrite(PUL, HIGH);
-  digitalWrite(DIR, LOW);
+  digitalWrite(PUL, HIGH); // default speed 0
+  digitalWrite(DIR, LOW); // default direction CCW
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  output = 0.5;
-  writePercent(output);
+  current_time = micros();
+  timer = (v_out == LOW) ? time_low : time_high;
+  if ((current_time - last_time) >= timer) { 
+    v_out = !v_out;
+    digitalWrite(PUL, v_out);
+    last_time = current_time;
+  }
+
+  // Serial.println(time_high);
+  // writePercent(output);
 }
 
 void writePercent(float value) {
@@ -26,10 +41,8 @@ void writePercent(float value) {
     digitalWrite(DIR, HIGH); // spin CW for positive values
   }
 
-  // convert value to proportion of max duty cycle, then to integer between 0 and 255
-  // int actual_output = int(value * MAX_DUTY_CYCLE * 255); 
-  // analogWrite(PUL, actual_output);
-  float high = 3 / (abs(value)*0.15);
+  // convert value to proportion of max duty cycle, then to duty cycle delay
+  float high = 3 / (abs(value) * MAX_DUTY_CYCLE);
   digitalWrite(PUL, LOW);
   delayMicroseconds(3); // Minimum 2.5us
   digitalWrite(PUL, HIGH);
